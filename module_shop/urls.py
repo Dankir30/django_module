9ae3e0b128436dfa10d49d51af5daf001126e00e
Dankir30 <kirrdann@gmail.com>
2023-05-15 21:03:57 +0300

    first attempt module

--- module_shop/urls.py
+++ module_shop/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop_app.views import ProductListView, ProductDetailView, MyLoginView, RegisterView, MyLogout, ProductCreateView, \
+    BuyItemListView, BuyItemCreateView, BuyItemReturnListView, ProductUpdateView, BuyItemReturnCreateView, \
+    ApproveReturnView, DeclineReturnView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ProductListView.as_view(), name='products'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='details'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', MyLogout.as_view(), name='logout'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+    path('buy_items/', BuyItemListView.as_view(), name='buy_items'),
+    path('new_buy_item/<int:pk>', BuyItemCreateView.as_view(), name='new_buy_item'),
+    path('returns/', BuyItemReturnListView.as_view(), name='returns'),
+    path('edit/product/<int:pk>', ProductUpdateView.as_view(), name='edit'),
+    path('new_return/', BuyItemReturnCreateView.as_view(), name='new buyItem return'),
+    path('delete_buy_item_return/<int:pk>', DeclineReturnView.as_view(), name='delete_return'),
+    path('delete_buy_item/<int:pk>', ApproveReturnView.as_view(), name='delete_buy_item')
 ]
--- shop_app/admin.py
+++ shop_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, BuyItem, BuyItemReturn, MyUser
 
+admin.site.register(Product)
+admin.site.register(BuyItem)
+admin.site.register(BuyItemReturn)
+admin.site.register(MyUser)
 # Register your models here.
