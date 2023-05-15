463385d1426d5ed0cbaaa3991b301b977dafa578
Dankir30 <kirrdann@gmail.com>
2023-05-15 21:56:22 +0300

    updated_v2

--- module_shop/urls.py
+++ module_shop/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for module_shop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from shop_app.views import ProductListView, ProductDetailView, MyLoginView, RegisterView, MyLogout, ProductCreateView, \
@@ -35,5 +19,4 @@ urlpatterns = [
     path('new_return/', BuyItemReturnCreateView.as_view(), name='new buyItem return'),
     path('delete_buy_item_return/<int:pk>', DeclineReturnView.as_view(), name='delete_return'),
     path('delete_buy_item/<int:pk>', ApproveReturnView.as_view(), name='delete_buy_item')
-    
 ]
